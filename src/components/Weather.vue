2005701559bda5c037d13d59aae590f0e5483ba0
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-04-29 22:11:19 +0100

    Formatted data

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -4,18 +4,60 @@
     <div class = 'weather-data'>
       <div class='location'>Lat: {{lat}}, Lng: {{lng}}</div>
       <hr>
-      <pre>{{current}}</pre>
+      <div class="summary">
+        <div class='summary-head'>{{dailySummary.text}}</div>
+      </div>
+      <div class='current'>
+        <h2>Now</h2>
+        <div class='today-text'>{{current.summary}}</div>
+        <div class="temp">Temperature {{current.temp}}</div>
+        <div class="humidity">Humidity {{current.humidity}} %</div>
+        <div class="pressure">Air pressure {{current.pressure}}</div>
+        <div class="wind">Wind {{current.bearing}} &deg; / {{current.windspeed}} m/s</div>
+      </div>
+
       <hr>
-      <pre>{{dailySummary}}</pre>
+
+      <div class = 'hourly'>
+        <h2>The next hours</h2>
+        <ul>
+          <li v-for="hour in hourlyForecasts">
+            <h3 class="time">{{hour.time}}</h3>
+            <div class='summary'>{{hour.summary}}</div>
+            <div class="temp">Temperature {{hour.temp}} &deg;C</div>
+            <div class="humidity">Humidity {{hour.humidity}} %</div>
+            <div class="precip">Precimitation {{hour.precip}} mm (probability {{hour.preciProb}} %)</div>
+            <div class="wind">Wind {{hour.windBearing }} &deg; {{hour.windspeed}} m/s</div>
+          </li>
+        </ul>
+      </div>
+
       <hr>
-      <pre>{{dailyForecasts}}</pre>
-      <pre>{{hourlyForecasts}}</pre>
+
+      <div class='daily'>
+        <h2>This week</h2>
+        <ul>
+          <li v-for='daily in dailyForecasts' class='daily-forecast'>
+            <h3>{{daily.date}}</h3>
+            <div class="summary">{{daily.summary}}</div>
+            <div class='temp'>Temperature {{daily.minTemp}} - {{daily.maxTemp}} &deg; C</div>
+            <div class="humidity">Humidity {{daily.humidity}} %</div>
+            <div class="wind">Wind {{daily.bearing}} &deg; / {{daily.windspeed}} m/s</div>
+            <div class='clouds'>Cloudines {{daily.cloud}} %</div>
+            <div class='sunrise'>Sunrise {{daily.sunrise}}</div>
+            <div class='sunset'>Sunset {{daily.sunset}}</div>
+            <div class='moon'> Moon phase {{daily.moonPhase}} % </div>
+          </li>
+        </ul>
+      </div>
+      <hr>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import moment from 'moment'
 
 export default {
   data () {
@@ -77,18 +119,18 @@ export default {
           let dataItem = hourlyData[i]
           console.log('Hourly data item: ' + dataItem);
           let hourlyItem = {
-            clour: dataItem.cloudCover * 100,
-            humidity: dataItem.humidity * 100,
+            cloud: Math.round(dataItem.cloudCover * 100),
+            humidity: Math.round(dataItem.humidity * 100),
             icon: dataItem.icon,
-            ozone: dataItem.ozone,
-            precip: dataItem.precipIntensity,
-            preciProb: dataItem.precipProbability * 100,
-            pressure: dataItem.pressure,
-            summare: dataItem.summary,
-            temp: dataItem.temperature,
-            time: dataItem.time,
+            ozone: Math.round(dataItem.ozone),
+            precip: Math.round(dataItem.precipIntensity),
+            preciProb: Math.round(dataItem.precipProbability * 100),
+            pressure: Math.round(dataItem.pressure),
+            summary: dataItem.summary,
+            temp: Math.round(dataItem.temperature),
+            time: moment.unix(dataItem.time).format('ddd HH:mm'),
             windBearing: dataItem.windBearing,
-            windspeed: dataItem.windSpeed
+            windspeed: Math.round(dataItem.windSpeed)
           }
 
           this.hourlyForecasts.push(hourlyItem)
@@ -104,33 +146,36 @@ export default {
 
       let forecasts = daily.data
       console.log('Iterating Daily forecasts');
+      // TODO remove first item
       forecasts.map((forecast) => {
         let item = {
-          date: forecast.time,
+          date: moment.unix(forecast.time).format('ddd Do'),
           summary: forecast.summary,
-          maxTemp: forecast.apparentTemperatureMax,
-          minTemp: forecast.apparentTemperatureMin,
+          maxTemp: Math.round(forecast.apparentTemperatureMax),
+          minTemp: Math.round(forecast.apparentTemperatureMin),
+          cloud: Math.round(forecast.cloudCover * 100),
           humidity: forecast.humidity * 100,
           icon: forecast.icon,
-          windSpeed: forecast.windSpeed,
+          windSpeed: Math.round(forecast.windSpeed),
           bearing: forecast.windBearing,
-          ozon: forecast.ozone,
-          moon: forecast.moonPhase * 100,
-          cloud: forecast.cloudCover * 100
+          sunrise: moment.unix(forecast.sunriseTime).format('HH:mm'),
+          sunset: moment.unix(forecast.sunsetTime).format('HH:mm'),
+          moon: Math.round(forecast.moonPhase * 100),
         }
         this.dailyForecasts.push(item)
       })
     },
     parseCurrentWeatherData(current) {
+      console.log('Current', current);
       this.current = {
             summary: current.summary,
-            temp: current.temperature,
-            humidity: current.humidity * 100,
-            cloud: current.cloudCover * 100,
-            pressure: current.pressure,
+            temp: Math.round(current.temperature),
+            humidity: Math.round(current.humidity * 100),
+            cloud: Math.round(current.cloudCover * 100),
+            pressure: Math.round(current.pressure),
             bearing: current.windBearing,
-            windspeed: current.windSpeed,
-            ozone: current.ozone
+            windspeed: Math.round(current.windSpeed),
+            ozone: Math.round(current.ozone)
           }
     },
     getCurrentLocation() {
@@ -160,5 +205,14 @@ h1 {
 .weather-data {
   margin-left: 40px;
   margin-top: 20px;
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  padding: 10px 20px 0 10px;
+  border-radius: 2px solid black;
 }
 </style>
